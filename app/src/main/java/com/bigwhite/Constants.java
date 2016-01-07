883f7305f4244b6d46363ec429e53667e1f26c98
mee <mee>
2016-01-07 16:12:31 +0800

    first commit

---
+++ app/src/main/java/com/bigwhite/Constants.java
@@ -0,0 +1,14 @@
+package com.bigwhite;
+
+/**
+ * Created by dell on 2016/1/6 0006.
+ */
+public class Constants {
+    public static final boolean releaseModeOEM = true;
+    public static final String PREF_NAME = "ewhiteboard";
+    public static final String FILE_PATH = "电子课堂";
+    public static final String DIR_NAME = "板书";
+    public static final int BACK_COLOR = 0xFF005B3C;
+    // 状态栏高度48像素
+    public static final int STATUS_HEIGHT = 48;
+}
